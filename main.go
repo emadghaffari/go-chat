476f07deb21d6a281edf34592c00754eb4d79853
Emad <emadghaffariii@gmail.com>
2021-09-21 19:08:23 +0000

    call cmd for start application

--- main.go
+++ main.go
@@ -1,6 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"html"
+
+	"github.com/logrusorgru/aurora"
+
+	"github.com/emadghaffari/go-chat/cmd"
+)
+
 func main() {
-	
+	if err := cmd.Runner.RootCmd().Execute(); err != nil {
+		fmt.Printf("\n %v Failed to run command: %v %v\n\n ", aurora.White(html.UnescapeString("&#x274C;")), err, aurora.White(html.UnescapeString("&#x274C;")))
+	}
 }
 
